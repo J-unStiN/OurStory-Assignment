af6cc4aa66bc556d7f8356707527922555e43232
Park Sung Jun <junstin119@gmail.com>
2025-07-14 08:00:08 +0900

    [과제2] - first commit

---
+++ assign_2/assign_fcfs/src/main/kotlin/com/ourstory/assign_fcfs/AssignFcfsApplication.kt
@@ -0,0 +1,11 @@
+package com.ourstory.assign_fcfs
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AssignFcfsApplication
+
+fun main(args: Array<String>) {
+    runApplication<AssignFcfsApplication>(*args)
+}
---
+++ assign_2/assign_fcfs/src/test/kotlin/com/ourstory/assign_fcfs/AssignFcfsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ourstory.assign_fcfs
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AssignFcfsApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
